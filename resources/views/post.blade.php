e03b89658ea5d34b65704aaad1cedf08b90e4287
romanio <romatorlopov7@gmail.com>
2025-09-25 00:57:08 +0300

    Урок 15. View часть 2

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.main')
+@section('content')
     <div>
         @foreach($posts as $post)
             <div>{{ $post->title }}</div>
+            <div>{{ $post->content }}</div>
+            <div>{{ $post->likes }}</div>
         @endforeach
     </div>
-</body>
-</html>
+@endsection
